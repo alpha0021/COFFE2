0276dce1adeb5f6f8f0d6804ada066d1e58644b1
alpha0021 <rushdi_123@hotmail.com>
2018-06-13 22:43:11 +0200

    new folder added

---
+++ Entities/CoffeeEntity.php
@@ -0,0 +1,28 @@
+<?php 
+	
+	class CoffeEntity
+	{
+
+			public $id;
+			public $name;
+			public $type;
+			public $price;
+			public $roast;
+			public $country;
+			public $image;
+			public $review;
+
+			function_construct($id ,$name ,$type ,$price, $roast ,$country ,$image,$review){
+				$this->$id;
+				$this->$name;
+				$this->$type;
+				$this->$price;
+				$this->$roast;
+				$this->$country;
+				$this->$image;
+				$this->$review;
+			}
+
+	}
+
+?>
---
+++ model/CoffeeModel.php
@@ -0,0 +1,15 @@
+<?php 
+
+	require("Entities/CoffeeEntity.php");
+
+	//contains  database related code for the  coffee page
+
+	class CoffeeModel{
+		//Get all coffe types from the  database and  return them in an array
+
+		function GetCoffeeTypes(){
+			require 'Credentials.php';
+		}
+	}
+
+?>
